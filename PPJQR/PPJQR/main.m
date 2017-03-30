ae9d385a2f973f3af2bdaeb3bc10392c8478fa5e
1823105945 <1823105945@qq.com>
2017-03-30 23:10:49 +0800

    创建项目

---
+++ PPJQR/PPJQR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PPJQR
+//
+//  Created by liu_yakai on 17/3/30.
+//  Copyright © 2017年 liu_yakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
