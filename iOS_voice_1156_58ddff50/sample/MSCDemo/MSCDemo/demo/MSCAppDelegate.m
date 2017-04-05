f407147b3ea0cf71e5404716b1ad0c7c4f38bfa1
1823105945 <1823105945@qq.com>
2017-04-05 22:23:19 +0800

    集成科大讯飞语音包

--- iOS_voice_1156_58ddff50/sample/MSCDemo/MSCDemo/demo/MSCAppDelegate.m
+++ iOS_voice_1156_58ddff50/sample/MSCDemo/MSCDemo/demo/MSCAppDelegate.m
@@ -17,13 +17,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     
-    <#!!!特别提醒：                                                                #>
-    
-    <#  1、在集成讯飞语音SDK前请特别关注下面设置，主要包括日志文件设置、工作路径设置和appid设置。#>
-    
-    <#2、在启动语音服务前必须传入正确的appid。#>
-    
-    <#3、SDK运行过程中产生的音频文件和日志文件等都会保存在设置的工作路径下。#>
+//    <#!!!特别提醒：                                                                #>
+//    
+//    <#  1、在集成讯飞语音SDK前请特别关注下面设置，主要包括日志文件设置、工作路径设置和appid设置。#>
+//    
+//    <#2、在启动语音服务前必须传入正确的appid。#>
+//    
+//    <#3、SDK运行过程中产生的音频文件和日志文件等都会保存在设置的工作路径下。#>
     
     
     //设置sdk的log等级，log保存在下面设置的工作路径中
