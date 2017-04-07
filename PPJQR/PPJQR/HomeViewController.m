805b680fc675a09b85c2ed402dfd961a9e56fdf5
1823105945 <1823105945@qq.com>
2017-04-07 20:47:44 +0800

    麻痹科大讯飞平台二笔

--- PPJQR/PPJQR/HomeViewController.m
+++ PPJQR/PPJQR/HomeViewController.m
@@ -8,15 +8,224 @@
 
 #import "HomeViewController.h"
 #import "PPJQR.h"
+#import "iflyMSC/iflyMSC.h"
+#import "IATConfig.h"
+#import "ISRDataHelper.h"
 
-@interface HomeViewController ()
+@interface HomeViewController ()<IFlySpeechRecognizerDelegate,IFlyRecognizerViewDelegate,IFlyPcmRecorderDelegate>
 @property (nonatomic, strong) NSMutableArray *resultArray;
+@property (nonatomic, strong) IFlySpeechRecognizer *iFlySpeechRecognizer;
+@property (nonatomic,strong) IFlyPcmRecorder *pcmRecorder;
+@property (nonatomic, strong)IFlySpeechUnderstander *iFlySpeechUnderstander;
 @end
 
 @implementation HomeViewController
+/**
+ 设置识别参数
+ ****/
+-(void)initRecognizer
+{
+    NSLog(@"%s",__func__);
+    
+    
+        //单例模式，无UI的实例
+        if (_iFlySpeechRecognizer == nil) {
+            _iFlySpeechRecognizer = [IFlySpeechRecognizer sharedInstance];
+            
+            [_iFlySpeechRecognizer setParameter:@"" forKey:[IFlySpeechConstant PARAMS]];
+            
+            //设置听写模式
+            [_iFlySpeechRecognizer setParameter:@"iat" forKey:[IFlySpeechConstant IFLY_DOMAIN]];
+        }
+        _iFlySpeechRecognizer.delegate = self;
+        
+        if (_iFlySpeechRecognizer != nil) {
+            IATConfig *instance = [IATConfig sharedInstance];
+            
+            //设置最长录音时间
+            [_iFlySpeechRecognizer setParameter:instance.speechTimeout forKey:[IFlySpeechConstant SPEECH_TIMEOUT]];
+            //设置后端点
+            [_iFlySpeechRecognizer setParameter:instance.vadEos forKey:[IFlySpeechConstant VAD_EOS]];
+            //设置前端点
+            [_iFlySpeechRecognizer setParameter:instance.vadBos forKey:[IFlySpeechConstant VAD_BOS]];
+            //网络等待时间
+            [_iFlySpeechRecognizer setParameter:@"20000" forKey:[IFlySpeechConstant NET_TIMEOUT]];
+            
+            //设置采样率，推荐使用16K
+            [_iFlySpeechRecognizer setParameter:instance.sampleRate forKey:[IFlySpeechConstant SAMPLE_RATE]];
+            
+            if ([instance.language isEqualToString:[IATConfig chinese]]) {
+                //设置语言
+                [_iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+                //设置方言
+                [_iFlySpeechRecognizer setParameter:instance.accent forKey:[IFlySpeechConstant ACCENT]];
+            }else if ([instance.language isEqualToString:[IATConfig english]]) {
+                [_iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+            }
+            //设置是否返回标点符号
+            [_iFlySpeechRecognizer setParameter:instance.dot forKey:[IFlySpeechConstant ASR_PTT]];
+            
+        }
+        
+        //初始化录音器
+        if (_pcmRecorder == nil)
+        {
+            _pcmRecorder = [IFlyPcmRecorder sharedInstance];
+        }
+        
+        _pcmRecorder.delegate = self;
+        
+        [_pcmRecorder setSample:[IATConfig sharedInstance].sampleRate];
+        
+        [_pcmRecorder setSaveAudioPath:nil];    //不保存录音文件
+        
+   
+}
+//中文会话
+- (IBAction)conversation:(id)sender {
+    
+    if(_iFlySpeechRecognizer == nil)
+    {
+        [self initRecognizer];
+    }
+    
+    [_iFlySpeechRecognizer cancel];
+    
+    //设置音频来源为麦克风
+    [_iFlySpeechRecognizer setParameter:IFLY_AUDIO_SOURCE_MIC forKey:@"audio_source"];
+    
+    //设置听写结果格式为json
+    [_iFlySpeechRecognizer setParameter:@"json" forKey:[IFlySpeechConstant RESULT_TYPE]];
+    
+    //保存录音文件，保存在sdk工作路径中，如未设置工作路径，则默认保存在library/cache下
+    [_iFlySpeechRecognizer setParameter:@"asr.pcm" forKey:[IFlySpeechConstant ASR_AUDIO_PATH]];
+    
+    [_iFlySpeechRecognizer setDelegate:self];
+    
+    BOOL ret = [_iFlySpeechRecognizer startListening];
+    
+    if (ret) {
+//        [_audioStreamBtn setEnabled:NO];
+//        [_upWordListBtn setEnabled:NO];
+//        [_upContactBtn setEnabled:NO];
+        
+    }else{
+//        [_popUpView showText: @"启动识别服务失败，请稍后重试"];//可能是上次请求未结束，暂不支持多路并发
+    }
+}
+/**
+ 无界面，听写结果回调
+ results：听写结果
+ isLast：表示最后一次
+ ****/
+- (void) onResults:(NSArray *) results isLast:(BOOL)isLast
+{
+
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+    NSDictionary *dic = results[0];
+    for (NSString *key in dic) {
+        [resultString appendFormat:@"%@",key];
+    }
+    NSString * _result =[NSString stringWithFormat:@"%@%@", resultString];
+    NSString * resultFromJson =  [ISRDataHelper stringFromJson:resultString];
+    
+    if (isLast){
+//        NSLog(@"听写结果(json)：%@测试",  result);
+    }
+    NSLog(@"_result=%@",_result);
+    NSLog(@"resultFromJson=%@",resultFromJson);
+    
+}
+
+-(void)_init{
+    _iFlySpeechUnderstander = [IFlySpeechUnderstander sharedInstance];
+    _iFlySpeechUnderstander.delegate = self;
+    //设置为麦克风输入语音
+    [_iFlySpeechUnderstander setParameter:IFLY_AUDIO_SOURCE_MIC forKey:@"audio_source"];
+    
+    bool ret = [_iFlySpeechUnderstander startListening];
+    
+    if (ret) {
+        
+        
+        
+        
+    }
+    else
+    {
+//        [_popUpView showText: @"启动识别服务失败，请稍后重试"];//可能是上次请求未结束
+    }
+
+}
+
+//会话结束回调
+- (void) onError:(IFlySpeechError*) error{
+    NSLog(@"%s",__func__);
+    
+    NSString *text ;
+   
+    if (error.errorCode ==0 ) {
+       
+    }
+    else
+    {
+        text = [NSString stringWithFormat:@"发生错误：%d %@",error.errorCode,error.errorDesc];
+        NSLog(@"%@",text);
+    }
+    
+   
+
+}
+
+#pragma mark - IFlyPcmRecorderDelegate
+
+- (void) onIFlyRecorderBuffer: (const void *)buffer bufferSize:(int)size
+{
+    NSData *audioBuffer = [NSData dataWithBytes:buffer length:size];
+    
+    int ret = [self.iFlySpeechRecognizer writeAudio:audioBuffer];
+    if (!ret)
+    {
+        [self.iFlySpeechRecognizer stopListening];
+        
+        
+        
+    }
+}
+
+- (void) onIFlyRecorderError:(IFlyPcmRecorder*)recoder theError:(int) error
+{
+    
+}
+
+//power:0-100,注意控件返回的音频值为0-30
+- (void) onIFlyRecorderVolumeChanged:(int) power
+{
+    //    NSLog(@"%s,power=%d",__func__,power);
+    
+    
+    
+    NSString * vol = [NSString stringWithFormat:@"音量：%d",power];
+//    [_popUpView showText: vol];
+}
+
+//英文会话
+- (IBAction)enconversation:(id)sender {
+    [self _init];
+}
+//中译英
+- (IBAction)cnEntranslation:(id)sender {
+    
+}
+- (IBAction)Encntranslation:(id)sender {
+    
+}
+
+//英译中
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initRecognizer];
     NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:@"weixin",@"name",@"微信团队欢迎你。很高兴你开启了微信生活，期待能为你和朋友们带来愉快的沟通体检。",@"content", nil];
     NSDictionary *dict1 = [NSDictionary dictionaryWithObjectsAndKeys:@"rhl",@"name",@"hello",@"content", nil];
     NSDictionary *dict2 = [NSDictionary dictionaryWithObjectsAndKeys:@"rhl",@"name",@"0",@"content", nil];
